ad6a6a3e75ac15d80a1127019caf2208ac140031
Natali Schwartz <schwartznatali@gmail.com>
2025-12-08 21:25:22 -0300

    cambie la distribucion  de contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,19 +3,19 @@ import {Phone, Mail, MapPin} from 'lucide-react'
 
 const Contact = () => {
   return (
-    <section className="contact section py-16 dark:bg-dark-container-color" id="contact">
+    <section className="contact section pb-16 dark:bg-dark-container-color" id="contact">
       <div className="container mx-auto px-4 max-w-6xl">
         
         <h2 className="section__title text-2xl font-semibold md:text-4xl text-center mb-12 text-title-color dark:text-dark-title-color">
           Contacto
         </h2>
 
-        <div className="contact__container container flex-col items-start sm:flex gap-6 pb-4">
+        <div className="contact__container container flex flex-col items-start gap-6 pb-4">
           {/* Teléfono */}
           <div className="contact__information flex items-center gap-5">
             <Phone className='text-first-color text-4xl'/>
             <div className='flex-col'>
-              <h3 className="contact__title text-base font-medium text-title-color dark:text-dark-title-color mb-1">
+              <h3 className="contact__title text-base font-medium text-title-color dark:text-dark-title-color">
                 Teléfono
               </h3>
               <a 
@@ -31,7 +31,7 @@ const Contact = () => {
           <div className="contact__information flex items-center gap-5">
             <Mail className='text-first-color text-4xl'/>
             <div className='flex-col'>
-              <h3 className="contact__title text-base font-medium text-title-color dark:text-dark-title-color mb-1">
+              <h3 className="contact__title text-base font-medium text-title-color dark:text-dark-title-color">
                 Email
               </h3>
               <a 
@@ -47,7 +47,7 @@ const Contact = () => {
           <div className="contact__information flex items-center gap-5">
             <MapPin className='text-first-color text-4xl'/>
             <div className='flex-col'>
-              <h3 className="contact__title text-base font-medium text-title-color dark:text-dark-title-color mb-1">
+              <h3 className="contact__title text-base font-medium text-title-color dark:text-dark-title-color">
                 Ubicación
               </h3>
               <span className="contact__subtitle text-text-color-light text-sm">
